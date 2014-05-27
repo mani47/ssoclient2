664e4cce9171e0f7906fcfbca18d8e4efcd7a463
mani47 <mm.mani@gmail.com>
2014-05-27 15:42:01 +0430

    added ca-bundle

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -6,11 +6,7 @@ module Net
     alias_method :original_use_ssl=, :use_ssl=
 
     def use_ssl=(flag)
-      if Rails.application.config.ssl_ca_file == 'lib/ca-bundle.crt'
-        self.ca_file = Rails.root.join('lib/ca-bundle.crt').to_s
-      else
-        self.ca_file = Rails.application.config.ssl_ca_file 
-      end
+      self.ca_file = '/usr/lib/ssl/certs/ca-certificates.crt' 
       self.verify_mode = OpenSSL::SSL::VERIFY_PEER
       self.original_use_ssl = flag
     end
