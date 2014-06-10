53c4d208bdd71d2c332f0a63bdea20a0ac503c7a
mani47 <mm.mani@gmail.com>
2014-06-10 20:24:23 +0430

    added Google plus

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,24 @@
 class HomeController < ApplicationController
   
-  skip_before_filter :check_cas_login, :only => :public
-  
-  def public
+  def index
+    if request.session['cas'].nil? || request.session['cas']['user'].nil?
+      @user = nil
+    else
+      @user = request.session['cas']['user']
+    end
   end
   
-  def index
-    @user = request.session['cas']['user']
+  def login
+    if request.session['cas'].nil? || request.session['cas']['user'].nil?
+      return render :file => "#{Rails.root}/public/401.html", :status => 401, :layout => false
+    else
+      redirect_to '/index'
+    end
   end
   
   def logout
     request.session['cas'] = nil
     render 'index'
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 AccepttoSsoClient::Application.routes.draw do
-  root :to => 'home#public'
+  root :to => 'home#index'
   get 'index' => 'home#index'
   get 'logout' => 'home#logout'
+  get 'login' => 'home#login'
 end
